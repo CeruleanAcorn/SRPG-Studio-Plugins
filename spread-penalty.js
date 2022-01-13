4e8af3487a50dbb0d989064b5712cb7f8c010a88
CeruleanAcorn <73409410+CeruleanAcorn@users.noreply.github.com>
2022-01-13 15:37:19 -0800

    Updated spread-penalty.js, added weakest-link.js
    spread-penalty.js: Cleaned comments, spacing
    weakest-link.js: First commit

--- spread-penalty.js
+++ spread-penalty.js
@@ -1,8 +1,10 @@
-//Built off of MarkyJoe' Lone Wolf Plugin, and Claris' Redline Slayer and Distance Accuracy Plugin
-
 /*
-spread-penalty: With this skill, the user's hit rate and damage is affected by specified amounts 
-when attacking a unit further than a specified distance. The custom parameters involved in this skill are as follows:
+Using this plugin, through a skill with the keyword "spread-penalty" the user's hit rate and damage is affected by specified amounts 
+when attacking a unit further than a specified distance. Of note, these changes affect the unit's 
+stat(s) BEFORE factoring in their target's own stats. This means that the values that this plugin can change,
+as seen by default in the unit menu, are their "Atk" and "Hit", with the final values rounded down.
+
+The custom parameters involved in this skill are as follows:
 
 hitPenalty: The amount to alter the attacker's hit rate per tile distance exceeding distanceThreshold
 damagePenalty: The amount to alter the attacker's damage per tile distance exceeding distanceThreshold
@@ -13,13 +15,13 @@ By default, hitPenalty: 10, damagePenalty: 0.25, distanceThreshold: 1
 There are some important things to note about the custom parameters:
 - All must be a number of some sort.
 - distanceThreshold MUST be a whole number (no negative and decimal numbers)
-- The resulting values are rounded down using Math.floor();
 - hitPenalty and damagePenalty will be used in their corresponding calculations differently depending on if 
-the value given is an whole number or not. If the value is a whole number, the calculation will treat it as a 
-FLAT change where every tile beyond distanceThreshold applies the custom parameter to the corresponding stat. 
+the value given is a whole number or not. If the value is a whole number, the calculation will treat it as a 
+FLAT change where every tile beyond distanceThreshold adds the custom parameter to the corresponding stat. 
 If the value is a number that is NOT a whole number, the calculation will treat it as a PERCENTAGE change where the actual
 change is the value before the penalty minus the custom parameter multiplied by the number of tiles beyond distanceThreshold.
-For instance, a value of 2 means a -2 stat reduction per tile, while 0.2 means a 20% stat reduction per tile. 
+For instance, a value of 2 means a -2 stat reduction per exceeded tile, while 0.2 means a 20% stat reduction per exceeded tile. 
+- hitPenalty and damagePenalty, if negative, will INCREASE the corresponding stats instead of lowering them.
 
 Example declaration of custom parameter:
 "{hitPenalty: 20, damagePenalty: 0.25, distanceThreshold: 1}"
@@ -27,16 +29,13 @@ Example declaration of custom parameter:
 This means that the penalties will be applied to the attacker when they are attacking from more than 1 tile away.
 So at 2 range, their hit is decreased by 20 * (2 - 1) = 20, and their damage is decreased by 0.25 * (2 - 1) = 0.25.  
 
-Note the word "Penalty" in "hitPenalty" and "damagePenalty" - the number for each determines what is TAKEN
-AWAY from the corresponding stat. Therefore, if the number used is negative, that stat is INCREASED.
-This plugin therefore allows the creation of a weapon with a higher hit rate 
-but lower damage, higher damage but a lower hit rate, or both higher damage AND hit 
-rate from certain distances depending on which custom parameters are positive and negative.
+Special thanks to MarkyJoe and Claris, whose Lone Wolf and Redline Slayer/Distance Accuracy plugins, respectively, 
+I've adapted from in the making of this plugin.
 */
 
 (function() {
-	//Adapted from MarkyJoe's Lone Wolf Plugin.
-	//Returns distance between units instead of the number of units within a particular range
+	// Adapted from MarkyJoe's Lone Wolf Plugin.
+	// Returns distance between units instead of the number of units within a particular range
 	var getDistanceFromTarget = function(active, passive){
 		var unitX = active.getMapX();
 		var unitY = active.getMapY();
@@ -49,13 +48,13 @@ rate from certain distances depending on which custom parameters are positive an
 	
 var alias1 = SkillRandomizer.isCustomSkillInvokedInternal;
 SkillRandomizer.isCustomSkillInvokedInternal = function(active, passive, skill, keyword) {
-	if (keyword === 'spread-penalty') {//Always activates for now...
+	if (keyword === 'spread-penalty') {
 		return this._isSkillInvokedInternal(active, passive, skill);
 	}
 	return alias1.call(this, active, passive, skill, keyword);
 };
 
-//Adapted from Lady Rena's Redline Slayer plugin.
+// Adapted from Claris' Redline Slayer plugin.
 var alias2 = DamageCalculator.calculateAttackPower;
 DamageCalculator.calculateAttackPower = function(active, passive, weapon, isCritical, totalStatus, trueHitValue) {
 	var damage = alias2.call(this, active, passive, weapon, isCritical, totalStatus, trueHitValue);
@@ -67,18 +66,14 @@ DamageCalculator.calculateAttackPower = function(active, passive, weapon, isCrit
 		var y = active.getMapY();
 		var distance = getDistanceFromTarget(active, passive);
 
-		//Notice wording used being "Threshold"- no penalty is applied 
-		//Unless the distance EXCEEDS distanceThreshold
-		//Therefore for a penalty to begin at range 2, distanceThreshold should equal 1
 		if(distance > distanceThreshold){
-			if(damagePenalty % 1 != 0){//damagePenalty is NOT a whole number, calculate via percentage.
+			if(damagePenalty % 1 != 0){ // damagePenalty is NOT a whole number, calculate via percentage.
 				damage -= (damage * ((distance - distanceThreshold) * damagePenalty));
-			}else{//damagePenalty IS a whole number, calculate via flat value.
+			}else{ // damagePenalty IS a whole number, calculate via flat value.
 				damage -= ((distance - distanceThreshold) * damagePenalty);
 		}
-		root.log("Damage now: " + damage);
+		
 		if(damage < 0){
-			root.log("Negative: make 0!");
 			damage = 0;
 		}
 		}
@@ -86,7 +81,7 @@ DamageCalculator.calculateAttackPower = function(active, passive, weapon, isCrit
 	return Math.floor(damage);
 };
 
-    //Adapted from Claris' Distance Accuracy Plugin.
+    // Adapted from Claris' Distance Accuracy Plugin.
     var alias3 = HitCalculator.calculateSingleHit;
     HitCalculator.calculateSingleHit = function(active, passive, weapon, totalStatus) {
 	var hitRate = alias3.call(this,active,passive,weapon,totalStatus);
@@ -107,9 +102,9 @@ DamageCalculator.calculateAttackPower = function(active, passive, weapon, isCrit
 	var distance = getDistanceFromTarget(active, passive);
 
 	if(distance > distanceThreshold){
-		if(hitPenalty % 1 != 0){//If hitPenalty is NOT a whole number, calculate via percentage.
+		if(hitPenalty % 1 != 0){ // If hitPenalty is NOT a whole number, calculate via percentage.
 			hitRate -= (hitRate * ((distance - distanceThreshold) * hitPenalty));
-		}else{//hitPenalty IS a whole number, calculate via flat value.
+		}else{ // hitPenalty IS a whole number, calculate via flat value.
 		hitRate -= ((distance - distanceThreshold) * hitPenalty);
 		}
 		if(hitRate < 0){
---
+++ weakest-link.js
@@ -0,0 +1,44 @@
+/*
+With this skill, the lowest between the two of Defense and Resistance is used (after considering
+parameter bonuses) when calculating the damage an attack will deal, a la Three Houses.
+Which one is used when both are equal can be set using a custom parameter.
+By default, the stat attacked will depend on whatever the weapon naturally targets.
+HUGE thank you to Claris for their CL_DefSwap.js, an invaluable reference this plugin follows
+for using DamageCalculator.calculateDefense
+*/
+
+(function() {
+var alias1 = SkillRandomizer.isCustomSkillInvokedInternal;
+SkillRandomizer.isCustomSkillInvokedInternal = function(active, passive, skill, keyword) {
+	if (keyword === 'weakest-link') {
+		return this._isSkillInvokedInternal(active, passive, skill);
+	}
+	return alias1.call(this, active, passive, skill, keyword);
+}
+
+var alias2 = DamageCalculator.calculateDefense;
+DamageCalculator.calculateDefense = function(active, passive, weapon, isCritical, totalStatus, trueHitValue) {
+	
+	//Case of engine's Weapon Option for NOGUARD, covered in Claris' CL_ DefSwap function so it's covered here too.
+	if (this.isNoGuard(active, passive, weapon, isCritical, trueHitValue)) {
+		return 0;
+	}
+	
+	var skill = SkillControl.getPossessionCustomSkill(active,"weakest-link");
+	var defensiveStatUsed = alias2.call(this, active, passive, weapon, isCritical, totalStatus, trueHitValue);
+	
+	if (skill !== null){ //Do not calculate differently if skill is not available
+		var def = RealBonus.getDef(passive);
+		var res = RealBonus.getMdf(passive);
+		if (def > res){ //def higher: calculate with res
+			defensiveStatUsed = res;
+		} else if (res > def) { //res higher: calculate with def
+			defensiveStatUsed = def;
+		}
+	}
+	
+	defensiveStatUsed += CompatibleCalculator.getDefense(passive, active, ItemControl.getEquippedWeapon(passive)) + SupportCalculator.getDefense(totalStatus);
+	return defensiveStatUsed;
+}
+
+})();  
