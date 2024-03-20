59748914aa77e324b2377854622628650232c00b
CeruleanAcorn <73409410+CeruleanAcorn@users.noreply.github.com>
2024-03-19 17:13:38 -0700

    Adjusted indentation

--- only-retaliate/SkillControl-getPossessionCustomSkillNonWeapon.js
+++ only-retaliate/SkillControl-getPossessionCustomSkillNonWeapon.js
@@ -19,22 +19,21 @@ to get the final array of unit skills.
 Refer to the aforementioned methods, plus the declaration of the ObjectFlag object key, in singleton-skillcontrol.js.
 */
 SkillControl.getPossessionCustomSkillNonWeapon = function(unit, keyword){
-		/*
-		root.log statements show that objectFlag = 11101011.
-		In this context each digit in these binary sequences represents something related to the unit that is checked for skills
-		to put together the unit's complete array of skills.
-		If the digit is 1, that skill source is checked. If it is 0, it not checked.
-		The rightmost digit represents the unit's equipped weapon.
-		It is left as 0 in this case because of the absence of ObjectFlag.WEAPON, so the unit's equipped weapon isn't checked.
-		
-		Observe that the objectFlag variable declared in getSkillMixArray named here as defaultObjectFlag = 11101111.
-		It appears based on the hexadecimal values in ObjectFlag from singleton-skillcontrol.js, the leftmost 0 that persists in both
-		binary sequences represents a SKILL. That appears to not be relevant to this plugin.
-		*/
-		var objectFlag = ObjectFlag.UNIT | ObjectFlag.CLASS | ObjectFlag.ITEM | ObjectFlag.STATE | ObjectFlag.TERRAIN | ObjectFlag.FUSION;
-		// var defaultObjectFlag = ObjectFlag.UNIT | ObjectFlag.CLASS | ObjectFlag.WEAPON | ObjectFlag.ITEM | ObjectFlag.STATE | ObjectFlag.TERRAIN | ObjectFlag.FUSION;
-		// root.log("Binary used is: " + objectFlag.toString(2)); // toString(2) prints the number in binary
-		// root.log("Default Binary used is: " + defaultObjectFlag.toString(2));
-		var arr = this.getSkillObjectArray(unit, ItemControl.getEquippedWeapon(unit), SkillType.CUSTOM, keyword, objectFlag);
-		return this._returnSkill(SkillType.CUSTOM, arr);
-};	
+	/*
+	root.log statements show that objectFlag = 11101011.
+	In this context each digit in these binary sequences represents something related to the unit that is checked for skills
+	to put together the unit's complete array of skills.
+	If the digit is 1, that skill source is checked. If it is 0, it not checked.
+	The rightmost digit represents the unit's equipped weapon.
+	It is left as 0 in this case because of the absence of ObjectFlag.WEAPON, so the unit's equipped weapon isn't checked.
+	
+	Observe that the objectFlag variable declared in getSkillMixArray named here as defaultObjectFlag = 11101111.
+	It appears based on the hexadecimal values in ObjectFlag from singleton-skillcontrol.js, the leftmost 0 that persists in both
+	binary sequences represents a SKILL. That appears to not be relevant to this plugin.
+	*/
+	var objectFlag = ObjectFlag.UNIT | ObjectFlag.CLASS | ObjectFlag.ITEM | ObjectFlag.STATE | ObjectFlag.TERRAIN | ObjectFlag.FUSION;
+	// var defaultObjectFlag = ObjectFlag.UNIT | ObjectFlag.CLASS | ObjectFlag.WEAPON | ObjectFlag.ITEM | ObjectFlag.STATE | ObjectFlag.TERRAIN | ObjectFlag.FUSION;
+	// root.log("Binary used is: " + objectFlag.toString(2)); // toString(2) prints the number in binary
+	// root.log("Default Binary used is: " + defaultObjectFlag.toString(2));
+	var arr = this.getSkillObjectArray(unit, ItemControl.getEquippedWeapon(unit), SkillType.CUSTOM, keyword, objectFlag);
+	return this._returnSkill(SkillType.CUSTOM, arr);
--- only-retaliate/only-retaliate-skill-player.js
+++ only-retaliate/only-retaliate-skill-player.js
@@ -5,7 +5,7 @@ Version 1.0: 03/19/2024
 	- Initial Public Release
 
 ***DEPENDENCY NOTICE***
-This plugin needs only-retaliate-skill-getPossessionCustomSkillNonWeapon.js active to work!!
+This plugin needs SkillControl-getPossessionCustomSkillNonWeapon.js active to work!!
 ***********************
 
 With this plugin, if a weapon has a custom skill with the keyword "onlyRetaliate", 
@@ -63,58 +63,57 @@ We need to call this function with the hexadecimal value for the unit's equipped
 bitwise operation for checking weapons doesn't happen.
 */
 var isUnitAttackableAlias = AttackChecker.isUnitAttackable;
-	AttackChecker.isUnitAttackable = function(unit) {
-		var unitCanAttack = isUnitAttackableAlias.call(this, unit);
+AttackChecker.isUnitAttackable = function(unit) {
+	var unitCanAttack = isUnitAttackableAlias.call(this, unit);
+	
+	// If unit cannot already attack based on vanilla implementation attack checking, disregard further checking.
+	if(unitCanAttack){
+		// Even if there are weapons that do not have the keyword, if the unit possesses the skill, 
+		// don't allow checking at all.
 		
-		// If unit cannot already attack based on vanilla implementation attack checking, disregard further checking.
-		if(unitCanAttack){
-			// Even if there are weapons that do not have the keyword, if the unit possesses the skill, 
-			// don't allow checking at all.
-			
-			// A new function based on SkillControl.getPossessionCustomSkill is used for this plugin.
-			var unitSkill = SkillControl.getPossessionCustomSkillNonWeapon(unit, "onlyRetaliate");
-			if (unitSkill != null) {
-				// root.log("The unit itself has the skill. Preventing Attack command completely.");
-				return false;
-			}
-			
-			// root.log("Skill not found on unit, proceeding to weapon check.");
-				
-			// Unit does not have the skill without a weapon. Proceed to checking for weapons.
-			var i, item, indexArray;		
-			var count = UnitItemControl.getPossessionItemCount(unit);
-			for (i = 0; i < count; i++) {
-				item = UnitItemControl.getItem(unit, i);
-				if (item !== null && ItemControl.isWeaponAvailable(unit, item) && this._isWeaponEnabled(item)) { 
-					// The idea is that an item is added to indexArray if the unit can attack with it. 
-					// By skipping "indexArray = this.getAttackIndexArray(unit, item, true)" 
-					// via the continue statement, we do not add the item to the indexArray.
-					// root.log("Checking Item: " + item.getName());
-					if(doesItemHaveSpecificCustomSkill(item, "onlyRetaliate")){
-						// root.log("The item: " + item.getName() + " has the skill. Proceeding to next weapon check.");
-						continue;
-					}
- 
-					// Following declaration and if statement are from vanilla AttackChecker.isUnitAttackable.
-					// Checks that the weapon can attack another unit within range.
-					indexArray = this.getAttackIndexArray(unit, item, true);
-					if (indexArray.length !== 0) {
-						// root.log("A weapon without the skill was found. Allowing Attack command");
-						return true;
-					}
-				}
-			}
-			// root.log("All weapons have the skill. Preventing Attack command completely.");
+		// A new function based on SkillControl.getPossessionCustomSkill is used for this plugin.
+		var unitSkill = SkillControl.getPossessionCustomSkillNonWeapon(unit, "onlyRetaliate");
+		if (unitSkill != null) {
+			// root.log("The unit itself has the skill. Preventing Attack command completely.");
 			return false;
 		}
-		return unitCanAttack;
-	};
+		
+		// root.log("Skill not found on unit, proceeding to weapon check.");
+		// Unit does not have the skill without a weapon. Proceed to checking for weapons.
+		var i, item, indexArray;		
+		var count = UnitItemControl.getPossessionItemCount(unit);
+		for (i = 0; i < count; i++) {
+			item = UnitItemControl.getItem(unit, i);
+			if (item !== null && ItemControl.isWeaponAvailable(unit, item) && this._isWeaponEnabled(item)) { 
+				// The idea is that an item is added to indexArray if the unit can attack with it. 
+				// By skipping "indexArray = this.getAttackIndexArray(unit, item, true)" 
+				// via the continue statement, we do not add the item to the indexArray.
+				// root.log("Checking Item: " + item.getName());
+				if(doesItemHaveSpecificCustomSkill(item, "onlyRetaliate")){
+					// root.log("The item: " + item.getName() + " has the skill. Proceeding to next weapon check.");
+					continue;
+				}
+
+				// Following declaration and if statement are from vanilla AttackChecker.isUnitAttackable.
+				// Checks that the weapon can attack another unit within range.
+				indexArray = this.getAttackIndexArray(unit, item, true);
+				if (indexArray.length !== 0) {
+					// root.log("A weapon without the skill was found. Allowing Attack command");
+					return true;
+				}
+			}
+		}
+		// root.log("All weapons have the skill. Preventing Attack command completely.");
+		return false;
+	}
+	return unitCanAttack;
+};
 
 // Similar to AttackChecker.isAttackable but for Fusion Attacks.
 var isFusionAttackableAlias = FusionControl.isAttackable;
-	// Check if the unit can fuse with targetUnit based on fusionData.
-	FusionControl.isAttackable = function(unit, targetUnit, fusionData) {
-	var unitCanFusionAttack = isFusionAttackableAlias.call(this, unit, targetUnit, fusionData);
+// Check if the unit can fuse with targetUnit based on fusionData.
+FusionControl.isAttackable = function(unit, targetUnit, fusionData) {
+var unitCanFusionAttack = isFusionAttackableAlias.call(this, unit, targetUnit, fusionData);
 	if(unitCanFusionAttack){
 		var unitSkill = SkillControl.getPossessionCustomSkillNonWeapon(unit, "onlyRetaliate");
 		if (unitSkill != null) {
@@ -122,28 +121,27 @@ var isFusionAttackableAlias = FusionControl.isAttackable;
 			return false;
 		}
 		
-	// root.log("Skill not found on unit, proceeding to weapon check.");
-
-	var i, item, indexArray;		
-	var count = UnitItemControl.getPossessionItemCount(unit);
-	for (i = 0; i < count; i++) {
-		item = UnitItemControl.getItem(unit, i);
-		if (item !== null && ItemControl.isWeaponAvailable(unit, item) && AttackChecker._isWeaponEnabled(item)) { 
-			// root.log("Checking Item: " + item.getName());
-			if (doesItemHaveSpecificCustomSkill(item, "onlyRetaliate")) {
-				// root.log("The item: " + item.getName() + " has the skill. Proceeding to next weapon check.");
-				continue;
-			}
-			
-			indexArray = AttackChecker.getAttackIndexArray(unit, item, true);
-			if (indexArray.length !== 0) {
-				// root.log("A weapon without the skill was found. Allowing Fusion Attack command");
-				return true;
+		// root.log("Skill not found on unit, proceeding to weapon check.");
+		var i, item, indexArray;		
+		var count = UnitItemControl.getPossessionItemCount(unit);
+		for (i = 0; i < count; i++) {
+			item = UnitItemControl.getItem(unit, i);
+			if (item !== null && ItemControl.isWeaponAvailable(unit, item) && AttackChecker._isWeaponEnabled(item)) { 
+				// root.log("Checking Item: " + item.getName());
+				if (doesItemHaveSpecificCustomSkill(item, "onlyRetaliate")) {
+					// root.log("The item: " + item.getName() + " has the skill. Proceeding to next weapon check.");
+					continue;
+				}
+	
+				indexArray = AttackChecker.getAttackIndexArray(unit, item, true);
+				if (indexArray.length !== 0) {
+					// root.log("A weapon without the skill was found. Allowing Fusion Attack command");
+					return true;
+				}
 			}
 		}
-	}
-	// root.log("All weapons have the skill. Preventing Fusion Attack command completely.");
-	return false;
+		// root.log("All weapons have the skill. Preventing Fusion Attack command completely.");
+		return false;
 	}
 	return unitCanFusionAttack;
 };
@@ -151,17 +149,13 @@ var isFusionAttackableAlias = FusionControl.isAttackable;
 // Disallows a specific weapon from being a selectable item AFTER selecting the attack command.
 var isWeaponAllowedAlias = WeaponSelectMenu._isWeaponAllowed;
 WeaponSelectMenu._isWeaponAllowed = function(unit, item) {
-	
 	var weaponIsAllowed = isWeaponAllowedAlias.call(this, unit, item);
 	if(!weaponIsAllowed){
 		return weaponIsAllowed;
 	}
-	
 	if (doesItemHaveSpecificCustomSkill(item, "onlyRetaliate")){
 		// root.log("onlyRetaliate on weapon!");
 		return false;
-	}
-	
 	return true;
 };
 
@@ -172,13 +166,11 @@ FusionWeaponSelectMenu._isWeaponAllowed = function(unit, item) {
 		if(!fusionWeaponIsAllowed){
 			return fusionWeaponIsAllowed;
 		}
-		
 		if (doesItemHaveSpecificCustomSkill(item, "onlyRetaliate")){
 			// root.log("onlyRetaliate on fusion weapon!");
 			return false;
 		}
-
-		return true;
+	return true;
 };
 
 })()
